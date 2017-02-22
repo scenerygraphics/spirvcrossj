2221714b12af35cd28ecdb91026cb6bad83f3f27
Ulrik Günther <ulrik@42degreesoffreedom.com>
2017-02-22 13:41:56 +0100

    Bump to 0.2.3-SNAPSHOT and update of CI scripts and deployment scripts

---
+++ deploy/before-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_409548f45aa2_key -iv $encrypted_409548f45aa2_iv -in deploy/codesigning.asc.enc -out deploy/codesigning.asc -d
+    gpg --fast-import deploy/codesigning.asc
+fi
---
+++ deploy/deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+if [ "$TRAVIS_BRANCH" != 'master' ] || [ "$TRAVIS_PULL_REQUEST" == 'true' ]; then
+    echo "Not deploying, as we are on a feature branch or PR."
+    exit
+fi
+
+REPOSITORY_ID="ossrh"
+SNAPSHOT_REPOSITORY="https://oss.sonatype.org/content/repositories/snapshots"
+RELEASE_REPOSITORY="https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+
+VERSION=`MAVEN_OPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=WARN -Dorg.slf4j.simpleLogger.log.org.apache.maven.plugins.help=INFO" mvn help:evaluate -o -Dexpression=project.version | tail -1`
+PLATFORM=`mvn help:active-profiles | grep platform | awk '{print $2}'`
+
+if [ "$PLATFORM" == "platform-osx" ]
+then
+    CLASSIFIER="natives-macos"
+elif [ "$PLATFORM" == "platform-linux" ]
+then
+    CLASSIFIER="natives-linux"
+elif [ "$PLATFORM" == "platform-windows" ]
+then
+    CLASSIFIER="natives-windows"
+fi
+
+if [[ "$VERSION" == *"SNAPSHOT"* ]]
+then
+    echo "Configuring for SNAPSHOT deployment"
+    REPOSITORY=$SNAPSHOT_REPOSITORY
+    RELEASE_OPTS="-P sign"
+else
+    echo "Configuring for release deployment"
+    REPOSITORY=$RELEASE_REPOSITORY
+    RELEASE_OPTS="-P release,sign"
+fi
+
+echo "Deploying version $VERSION with classifier $CLASSIFIER to Sonatype..."
+
+mvn $RELEASE_OPTS deploy --settings settings.xml
+mvn deploy:deploy-file -DgroupId=graphics.scenery -Dversion=$VERSION \
+-DartifactId=spirvcrossj \
+-Dfile=target/spirvcrossj-$VERSION-$CLASSIFIER.jar \
+-DrepositoryId=$REPOSITORY_ID \
+-Durl=$REPOSITORY \
+-Dclassifier=$CLASSIFIER -DgeneratePom=false $RELEASE_OPTS --settings settings.xml

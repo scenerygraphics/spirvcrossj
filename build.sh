e6992107b9ba39a846428e5a6a60fcd45863ac38
Ulrik Guenther <hello@ulrik.is>
2018-10-24 02:37:19 +0200

    Update build scripts to include patching of generated Java classes

--- build.sh
+++ build.sh
@@ -4,12 +4,17 @@ shopt -s extglob
 SPIRVCROSSJ_DIR=`pwd`
 JAVA_DIR="$SPIRVCROSSJ_DIR/src/main/java/graphics/scenery/spirvcrossj/"
 
+echo "Cleaning old wrapper files ..."
 cd $JAVA_DIR
 rm -- !(Loader.java)
 cd $SPIRVCROSSJ_DIR
 
+echo "Initialising glslang ..."
 git submodule update
 cd $SPIRVCROSSJ_DIR/glslang && git apply ../fix-tokenizer.patch
+python ./update_glslang_sources.py
+
+echo "Building now ..."
 
 cd $SPIRVCROSSJ_DIR
 mkdir -p build
@@ -19,4 +24,5 @@ cmake ..
 cmake --build .
 
 cd $SPIRVCROSSJ_DIR
+./post-build.sh
 mvn clean package
---
+++ post-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+shopt -s extglob
+
+SPIRVCROSSJ_DIR=`pwd`
+JAVA_DIR="$SPIRVCROSSJ_DIR/src/main/java/graphics/scenery/spirvcrossj/"
+
+cd $JAVA_DIR
+find . -type f -name '*.java' -exec sed -i 's/spirv_cross::/Types./g' {} \;
+cd $SPIRVCROSSJ_DIR

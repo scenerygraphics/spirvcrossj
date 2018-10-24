689f78574dc82781b8934ca18c126ff37b43eaff
Ulrik Guenther <hello@ulrik.is>
2018-10-24 03:08:06 +0200

    Fix post-build script for macOS

--- post-build.sh
+++ post-build.sh
@@ -5,5 +5,5 @@ SPIRVCROSSJ_DIR=`pwd`
 JAVA_DIR="$SPIRVCROSSJ_DIR/src/main/java/graphics/scenery/spirvcrossj/"
 
 cd $JAVA_DIR
-find . -type f -name '*.java' -exec sed -i 's/spirv_cross::/Types./g' {} \;
+find . -type f -name '*.java' -exec sed -i"" 's/spirv_cross::/Types./g' {} \;
 cd $SPIRVCROSSJ_DIR

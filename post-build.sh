84f6452705a5c2b6f06897809458249514db57c7
Ulrik Guenther <hello@ulrik.is>
2018-10-24 03:11:48 +0200

    Make post-build script more portable and add debug output

--- post-build.sh
+++ post-build.sh
@@ -4,6 +4,20 @@ shopt -s extglob
 SPIRVCROSSJ_DIR=`pwd`
 JAVA_DIR="$SPIRVCROSSJ_DIR/src/main/java/graphics/scenery/spirvcrossj/"
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     FIND_CMD=find;;
+    Darwin*)    FIND_CMD=find;;
+    CYGWIN*)    FIND_CMD=find;;
+    MINGW*)     FIND_CMD=/bin/find;;
+    *)          FIND_CMD=find
+esac
+
+echo "Using find ($FIND_CMD) on $unameOut."
+
+echo "Patching wrapped classes..."
 cd $JAVA_DIR
-find . -type f -name '*.java' -exec sed -i"" 's/spirv_cross::/Types./g' {} \;
+$FIND_CMD . -type f -name '*.java' -exec sed -i".bak" 's/spirv_cross::/Types./g' {} \;
 cd $SPIRVCROSSJ_DIR
+
+echo "Continueing build..."
